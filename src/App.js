ababa2a33d3f5eb967492caff8f372fe10d5cc7b
Jean M Cetoute <jeanmarylerate51@gmail.com>
2019-08-20 13:22:27 -0500

    add the state on the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<table className="titleBar">
-				<tbody>
-					<tr>
-						<td>
-							<img width="50" src="moviedb.png" alt="website logo" />
-						</td>
-						<td>
-							<h1>MoviesDB Search</h1>{' '}
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		const movies = [
+			{
+				id: 0,
+				title: 'Avengers: Infinity War',
+				overview:
+					'As the Avengers and their allies have continued to protect the world from threat too large for any one hero to handle,a new danger has emerged frm the cosmic shadows: Thanos.',
+				id: 0,
+				title: 'Infinity War',
+				overview:
+					' and their allies have continued to protect the world from threat too large for any one hero to handle,a new danger has '
+			}
+		];
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<table className="titleBar">
+					<tbody>
+						<tr>
+							<td>
+								<img width="50" src="moviedb1.png" alt="app-icon" />
+							</td>
+							<td>
+								<h1>MoviesDB Search</h1>{' '}
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<input className="input" placeholder="Enter search here" />
+				{this.state.rows}
+			</div>
+		);
+	}
 }
 
 export default App;
